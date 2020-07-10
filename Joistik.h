31dbf860837b5e1cf12381ea7d672fe4cb213aee
MehmetNurErik-arduino <68108276+MehmetNurErik-arduino@users.noreply.github.com>
2020-07-10 14:17:04 +0300

    Update Joistik.h

--- Joistik.h
+++ Joistik.h
@@ -1,1 +1,18 @@
 
+
+#ifndef JOISTIK_H
+#define JOISTIK_H
+
+#include "Arduino.h"
+
+class JoistikPin {
+ public:
+    JoistikPin(int EksenX, int EksenY);
+    double uzaklikCm();
+    double uzaklikCm(float temperature);
+ private:
+    int EksenX, EksenY;
+};
+
+#endif // JOISTIK_H
+
